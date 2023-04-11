9c03742712ea1147bf84e0c0f590f63aecf6f798
mparker42 <mparker42@hotmail.co.uk>
2023-04-11 20:43:26 +0100

    Initial commit, first failing test

---
+++ MP.SampleCode.StringCalculator.Tests/Services/StringParserServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using MP.SampleCode.StringCalculator.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.SampleCode.StringCalculator.Tests.Services
+{
+    [TestClass]
+    public class StringParserServiceTests
+    {
+        private StringParserService _classUnderTest;
+
+        public StringParserServiceTests()
+        {
+            _classUnderTest = new StringParserService();
+        }
+
+        // First test as the start of code that confirms an empty string is treated as an array with one zero in it.
+        [TestMethod]
+        [DataRow("", DisplayName = "Confirm an empty string parses as an array with one zero in it")]
+        [DataRow(null, DisplayName = "Confirm null parses as an array with one zero in it")]
+        public void EmptyStringParsesAsTest(string? testValue)
+        {
+            var result = _classUnderTest.ParseValue("");
+
+            CollectionAssert.AreEquivalent(new[] { 0 }, result);
+        }
+    }
+}
