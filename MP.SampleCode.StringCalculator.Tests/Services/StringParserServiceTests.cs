eb0ad6feb53799dab660c3cb71c6f3d7d920c420
mparker42 <mparker42@hotmail.co.uk>
2023-04-11 21:05:39 +0100

    Add a test for parsing one number

--- MP.SampleCode.StringCalculator.Tests/Services/StringParserServiceTests.cs
+++ MP.SampleCode.StringCalculator.Tests/Services/StringParserServiceTests.cs
@@ -21,11 +21,33 @@ namespace MP.SampleCode.StringCalculator.Tests.Services
         [TestMethod]
         [DataRow("", DisplayName = "Confirm an empty string parses as an array with one zero in it")]
         [DataRow(null, DisplayName = "Confirm null parses as an array with one zero in it")]
-        public void EmptyStringParsesAsTest(string? testValue)
+        public void EmptyStringParsesAsZeroTest(string? testValue)
         {
             var result = _classUnderTest.ParseAsArrayOfNumbers(testValue);
 
             CollectionAssert.AreEquivalent(new[] { 0 }, result);
+        }
+
+        public static IEnumerable<object[]> SingleNumberTestData
+        {
+            get
+            {
+                // Without an explicit requirement, test every number up to 100.
+                for (var i = 0; i < 100; i++)
+                {
+                    yield return new object[] { i.ToString(), i };
+                }
+            }
+        }
+
+        // Second test, passing one number is treated as an array with the passed number in it.
+        [TestMethod]
+        [DynamicData(nameof(SingleNumberTestData))]
+        public void StringWithOneNumberParsesAsOneNumberTest(string? testValue, int expectedResult)
+        {
+            var result = _classUnderTest.ParseAsArrayOfNumbers(testValue);
+
+            CollectionAssert.AreEquivalent(new[] { expectedResult }, result);
         }
     }
 }
